12f7ed2441a2cdf15572e2806fe3fc5ccde77c97
acappuccio <acappuccio@bancocredicoop.coop>
2025-05-27 12:36:42 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,3 @@
-import { Header } from "../components/header"
-import { HeroSection } from "../components/hero-section"
-import { ProcessSteps } from "../components/steps"
-import { StatsSection } from "../components/stats"
-import { ReviewsSection } from "../components/reviews"
-import { CTASection } from "../components/cta-section"
-import { Footer } from "../components/footer"
-
-export default function FitnessCoachLanding() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1">
-        <HeroSection />
-        <ProcessSteps />
-        <StatsSection />
-        <ReviewsSection />
-        <CTASection />
-      </main>
-      <Footer />
-    </div>
-  )
+export default function Home() {
+  return <h1>Test funcionando</h1>
 }
